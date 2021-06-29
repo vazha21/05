ae17a6e91d229f8da18663d35a8931247a3f80f5
Vazha Abdushelishvili <Vabdushelishvili@tbcbank.com.ge>
2021-06-29 17:13:21 +0400

    a

---
+++ index.js
@@ -0,0 +1,41 @@
+const table = document.getElementById("table");
+const error = document.getElementById("error");
+
+const map = new Map([
+  ["id", "id"],
+  ["year", "year"],
+  ["color", "color"],
+  ["name", "name"],
+  ["pantone_value", "value"],
+]);
+
+fetch("https://reqres.in/api/unknown")
+  .then((result) => result.json())
+  .then((data) => populateTable(data))
+  .catch(() => {
+    error.style.display = "block";
+  });
+
+function populateTable(result) {
+  const data = result.data;
+  data.forEach((item, index) => {
+    if (index === 0) {
+      // create header.
+      const tr = document.createElement("tr");
+      for (const key in item) {
+        const td = document.createElement("td");
+        td.innerText = map.get(key);
+        tr.appendChild(td);
+      }
+      table.appendChild(tr);
+    }
+    // creating rows
+    const tr = document.createElement("tr");
+    for (const key in item) {
+      const td = document.createElement("td");
+      td.innerText = item[key];
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  });
+}
